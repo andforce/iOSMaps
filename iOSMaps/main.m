5d4f7108593404717c1a4d3d8340317a70f90e47
andforce <86118@163.com>
2015-12-04 17:25:28 +0800

    first

---
+++ iOSMaps/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSMaps
+//
+//  Created by WDY on 15/12/4.
+//  Copyright © 2015年 andforce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
