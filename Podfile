df3b3a83d8bd30afd13d3ac1b0a2d117e54ea0ae
andforce <86118@163.com>
2015-12-05 12:48:53 +0800

    add navi

--- Podfile
+++ Podfile
@@ -1,4 +1,5 @@
 platform :ios, '9.0' #手机的系统
-pod 'AMap3DMap'  #3D地图SDK
+#pod 'AMap3DMap'  #3D地图SDK
 pod 'AMapSearch' #搜索服务SDK
 pod 'AMapLocation' #定位SDK
+pod 'AMapNavi'  #导航SDK
--- iOSMaps/ViewController.m
+++ iOSMaps/ViewController.m
@@ -7,21 +7,20 @@
 //
 
 #import "ViewController.h"
-#import <MAMapKit/MAMapKit.h>
+
 
 #import "SearchView.h"
-
 #import "LocationView.h"
 
-
 #import <AMapLocationKit/AMapLocationKit.h>
+#import <AMapNaviKit/AMapNaviKit.h>
+#import <AMapSearchKit/AMapSearchKit.h>
 
 
-//#import "Mamap"
-
-@interface ViewController ()<MAMapViewDelegate ,AMapLocationManagerDelegate>{
+@interface ViewController ()<MAMapViewDelegate ,AMapLocationManagerDelegate, AMapSearchDelegate>{
     MAMapView *_mapView;
     AMapLocationManager *_locationManager;
+    AMapSearchAPI *_search;
     
     
 }
@@ -34,20 +33,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    // 定位
     [AMapLocationServices sharedServices].apiKey =@"f6a86f7a8d176d2ee0ae836d9e580158";
-    
-    //配置用户Key
-    [MAMapServices sharedServices].apiKey = @"f6a86f7a8d176d2ee0ae836d9e580158";
-    
-
     _locationManager = [[AMapLocationManager alloc]init];
     _locationManager.delegate = self;
     
     
-
-    
+    // 3D地图
+    [MAMapServices sharedServices].apiKey = @"f6a86f7a8d176d2ee0ae836d9e580158";
     _mapView = [[MAMapView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds))];
     _mapView.delegate = self;
+
+    // 搜索
+    [AMapSearchServices sharedServices].apiKey = @"f6a86f7a8d176d2ee0ae836d9e580158";
+    _search = [[AMapSearchAPI alloc]init];
+    _search.delegate = self;
+    
+    
+    //构造AMapPOIAroundSearchRequest对象，设置周边请求参数
+    AMapPOIAroundSearchRequest *request = [[AMapPOIAroundSearchRequest alloc] init];
+    request.location = [AMapGeoPoint locationWithLatitude:39.990459 longitude:116.481476];
+    request.keywords = @"方恒";
+    // types属性表示限定搜索POI的类别，默认为：餐饮服务|商务住宅|生活服务
+    // POI的类型共分为20种大类别，分别为：
+    // 汽车服务|汽车销售|汽车维修|摩托车服务|餐饮服务|购物服务|生活服务|体育休闲服务|
+    // 医疗保健服务|住宿服务|风景名胜|商务住宅|政府机构及社会团体|科教文化服务|
+    // 交通设施服务|金融保险服务|公司企业|道路附属设施|地名地址信息|公共设施
+    request.types = @"餐饮服务|生活服务";
+    request.sortrule = 0;
+    request.requireExtension = YES;
+    
+    //发起周边搜索
+    [_search AMapPOIAroundSearch: request];
+    
+    
+
+    
+
     
 
     [self.view addSubview:_mapView];
@@ -65,6 +87,25 @@
     
     [self startLocation];
     
+}
+
+
+-(void)onPOISearchDone:(AMapPOISearchBaseRequest *)request response:(AMapPOISearchResponse *)response{
+        if(response.pois.count == 0)
+        {
+            return;
+        }
+    
+        //通过 AMapPOISearchResponse 对象处理搜索结果
+        NSString *strCount = [NSString stringWithFormat:@"count: %ld",response.count];
+    
+        NSString *strSuggestion = [NSString stringWithFormat:@"Suggestion: %@", response.suggestion];
+        NSString *strPoi = @"";
+        for (AMapPOI *p in response.pois) {
+            strPoi = [NSString stringWithFormat:@"%@\nPOI: %@", strPoi, p.description];
+        }
+        NSString *result = [NSString stringWithFormat:@"%@ \n %@ \n %@", strCount, strSuggestion, strPoi];
+        NSLog(@"Place: %@", result);
 }
 
 -(void)startLocation{
