a31b84524d4cbf0ff6f8414f917731e446ae1afa
andforce <86118@163.com>
2015-12-20 16:31:02 +0800

    add navigationbatr

---
+++ iOSMaps/MapSettingNavigationViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapSettingNavigationViewController.h
+//  iOSMaps
+//
+//  Created by 迪远 王 on 15/12/20.
+//  Copyright © 2015年 andforce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapSettingNavigationViewController : UINavigationController
+
+@end
